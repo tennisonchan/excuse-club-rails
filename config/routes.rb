8dbf38ecf037fd0a13a3649222257c94b261dd95
pinglamb <pinglambs@gmail.com>
2016-09-10 22:37:47 -0700

    Added buddies api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, only: %i(create)
-    resources :buddies, only: %i(create)
+    resources :buddies, only: %i(index create)
     resources :excuses, only: %i(create)
     resources :begs, only: %i(index show) do
       member do
