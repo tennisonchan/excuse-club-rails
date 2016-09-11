c3b0eb44191b521c7e547e6ecb573f1464657134
pinglamb <pinglambs@gmail.com>
2016-09-10 23:17:04 -0700

    Make user registration ok to respond user if it is found by phone number

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,12 +2,8 @@ class Api::UsersController < ApplicationController
   skip_before_action :authenticate_user!
 
   def create
-    @user = User.new(user_params.merge(password: Devise.friendly_token(20), confirmed_at: Time.zone.now))
-    if @user.save
-      render json: @user
-    else
-      render json: { error: @user.errors.full_messages.first }
-    end
+    @user = User.where(phone: user_params[:phone]).first_or_create(user_params.merge(password: Devise.friendly_token(20), confirmed_at: Time.zone.now))
+    render json: @user
   end
 
   private
